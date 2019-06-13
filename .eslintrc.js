8b4164625ac98fd6bf51e560616eea1a2e613d40
DrugsZ <1002881508@qq.com>
2019-06-14 01:37:56 +0800

    chore:增加环境变量

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
     "extends": "airbnb",
+    "env": {
+      "browser": true,
+      "node": true
+    },
     "rules":{
       "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
     }
