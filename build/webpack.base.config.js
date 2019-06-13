3a1da7053c75e0244d7c64c678e546e2b0e250f7
DrugsZ <1002881508@qq.com>
2019-06-14 01:37:35 +0800

    chore:添加babel-loader,支持jsx解析

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -2,13 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const path = require('path');
 
-console.log(__dirname);
-
 module.exports = {
-  entry: './src/App.js',
+  entry: './src/App.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'my-first-webpack.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
