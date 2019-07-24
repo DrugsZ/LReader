ee661f16cc9c23c686725c6761cf7a34ee2c4251
DrugsZ <1002881508@qq.com>
2019-07-24 23:21:53 +0800

    style(core): Adjust CSS files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { render } from "react-dom";
 import React from "react";
 import Sliders from "./Layout";
-import "./App.less";
+import "./index.less";
 
-render(<Sliders width={48} />, document.getElementById("app"));
+render(<Sliders width={160} />, document.getElementById("app"));
