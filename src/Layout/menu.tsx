a759c861594b5ce41555857f056a926ef2d8bf48
DrugsZ <1002881508@qq.com>
2019-08-01 01:38:24 +0800

    refactor(layout): split menu.tsx add some feature

--- src/Layout/menu.tsx
+++ src/Layout/menu.tsx
@@ -1,20 +1,18 @@
 import * as React from "react";
-import * as PropTypes from "prop-types";
 import classNames from "classnames";
 import { Icon } from "antd";
+import MenuGroup, { CloneMenuProps } from "./menuGroup";
 
 const { useState } = React;
 const IconFont = Icon.createFromIconfontCN({
-  scriptUrl: "//at.alicdn.com/t/font_1318803_03j3hqvhz1wp.js"
+  scriptUrl: "//at.alicdn.com/t/font_1318803_03j3hqvhz1wp.js",
 });
 
 export interface MenuItem {
   logo: string;
   name: string;
-}
-
-interface MenuGroupProps {
-  children?: React.ReactNode;
+  select: boolean;
+  onClick?: (selectKey: number | string, e?: React.ChangeEvent) => void;
 }
 
 interface MenuState {
@@ -22,19 +20,20 @@ interface MenuState {
 }
 
 export const Menu = (props: MenuItem): JSX.Element => {
-  const { logo, name } = props;
+  const { logo, name, select, onClick } = props;
   const rxTwoCNChar = /^[\u4e00-\u9fa5]{2}$/;
   const isTwoCNChar = rxTwoCNChar.test.bind(rxTwoCNChar);
-  const [hasTwoCnChar, setHasTwoCnChar] = useState<MenuState>({
-    hasTwoCnChar: !!isTwoCNChar(name)
+  const [hasTwoCnChar] = useState<MenuState>({
+    hasTwoCnChar: !!isTwoCNChar(name),
   });
   return (
-    <div className="menu-item">
+    <div className="menu-item" role="presentation" onClick={onClick.bind(null, selectKey)}>
       <IconFont className="logo" type={logo} />
       <span
         className={classNames({
           "two-chinese-chars": hasTwoCnChar,
-          title: true
+          title: true,
+          isActive: select,
         })}
       >
         {name}
@@ -42,16 +41,5 @@ export const Menu = (props: MenuItem): JSX.Element => {
     </div>
   );
 };
-
-const MenuGroup: React.SFC<MenuGroupProps> = props => {
-  const { children } = props;
-  return (
-    <div className="menu-group">
-      {React.Children.map(children, menu => menu)}
-    </div>
-  );
-};
-
-MenuGroup.propTypes = { children: PropTypes.node.isRequired };
 
 Menu.Group = MenuGroup;
---
+++ src/Layout/menuGroup.tsx
@@ -0,0 +1,61 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+
+interface MenuGroupProps {
+  defaultSelectedKey?: number;
+}
+
+interface MenuGroupState {
+  selectKey: number | string;
+}
+
+export interface CloneMenuProps {
+  select: boolean;
+  onClick?: (selectKey: number | string, e?: React.ChangeEvent) => void;
+}
+
+/**
+ * 从props中解析默认选中菜单
+ * @param props 当前props
+ */
+function resloveProps(props: MenuGroupProps) {
+  const { defaultSelectedKey = "" } = props;
+  return defaultSelectedKey;
+}
+
+class MenuGroup extends React.Component<MenuGroupProps, MenuGroupState> {
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+  };
+
+  readonly state: MenuGroupState = {
+    selectKey: resloveProps(this.props),
+  };
+
+  /**
+   * 子菜单点击处理函数
+   * @param selectKey 触发点击事件的菜单索引
+   */
+  handleSelect(selectKey: number | string) {
+    this.setState({
+      selectKey,
+    });
+  }
+
+  renderMenuItem(children: React.ReactNode) {
+    const { selectKey } = this.state;
+    return React.Children.map(children, (menu, i) => {
+      return React.cloneElement(menu as React.ComponentElement<CloneMenuProps, any>, {
+        select: selectKey === i,
+        onClick: this.handleSelect.bind(this),
+      });
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    return <div className="menu-group">{this.renderMenuItem(children)}</div>;
+  }
+}
+
+export default MenuGroup;
